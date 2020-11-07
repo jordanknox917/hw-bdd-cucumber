aed091bc88105e27c0e5a67f44c1afeccac2a5b5
Jordan Knox <jordanknox100@berkeley.edu>
2020-11-07 03:19:46 +0000

    i have literally not tested a single thing

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -3,9 +3,10 @@
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
+    Movie.create(movie)
     # you should arrange to add that movie to the database here.
   end
-  fail "Unimplemented"
+#   fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +19,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+    expect page.body =~ /#{e1}.+#{e2}/m
+#   fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -27,12 +29,17 @@ end
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
+  rating_list.split(', ').each do |rating|
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+    uncheck ? uncheck("ratings_#{rating}") : check("ratings_#{rating}")
+  end
+#   fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  rows = page.all("#movies tr")
+  expect(Movie.all.size + 1).to eq(rows.size)
+#   fail "Unimplemented"
 end
